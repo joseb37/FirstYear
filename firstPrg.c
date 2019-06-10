e7243b472881cb8fb2ec6e17c20bd18b286e11cd
MattBelanger321 <39938415+MattBelanger321@users.noreply.github.com>
2019-06-10 14:34:59 -0400

    Delete firstPrg.c

--- firstPrg.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-/* Matthew Belanger 105016182
-   2018-09-25
-   My First Program */  
-
-int main(void) {
-	printf("*\n**\n***\nMy name is Matthew Belanger.\n***\n**\n*");
-	return 0;
-}
